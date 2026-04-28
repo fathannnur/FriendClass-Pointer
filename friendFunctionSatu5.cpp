c8507968f2fc4784512d71366d4a70bc85997b62
fathannnur <carelius02@gmail.com>
2026-04-28 19:12:07 +0700

    Fix variable name errors in setNama function and ensure proper output in friendFunctionSatu5.cpp

--- friendFunctionSatu5.cpp
+++ friendFunctionSatu5.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class mahasiswa
+{
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    at8.nama = b;
+    cput << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
